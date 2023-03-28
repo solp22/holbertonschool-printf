7027a8638ce86c80d13e969f697b40ea8282dd3b
JoaquinGarcia2408 <Joacogarcia2408@gmail.com>
2023-03-28 06:46:04 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * library
+ **/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+/**
+ * Prototypes
+ **/
+int _printf(const char *format, ...);
+#endif
