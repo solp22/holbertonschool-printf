74de9da4241a18a97c650df0961e3cb30a96d0c4
solp22 <sol.puentereal@gmail.com>
2023-03-28 07:05:28 -0700

    aesthetic fixes

--- main.h
+++ main.h
@@ -1,14 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * library
- **/
+
+/**Libraries**/
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
-/**
- * Prototypes
- **/
+
+/**Prototypes**/
 int _printf(const char *format, ...);
+
 #endif
